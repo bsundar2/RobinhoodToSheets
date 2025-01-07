fe7d40c2bf1561926eacda6415ffa2278653e439
Balaji Sundaram <basundar2@gmail.com>
2025-01-06 17:37:38 -0800

    Update robin_stocks version

--- lambda_function.py
+++ lambda_function.py
@@ -2,4 +2,5 @@ from src.rh_portfolio_to_sheets import export_rh_portfolio_to_sheets
 
 
 def lambda_handler(event, context):
+    print("Running lambda function")
     export_rh_portfolio_to_sheets(is_live=True, write_mock=False)
